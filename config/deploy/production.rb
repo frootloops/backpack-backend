d1779fae497c94844bc2a7a56bef0bba21487950
Arsen Gasparyan <to.arsen.gasparyan@gmail.com>
2015-10-18 20:55:29 +0300

    Update puma config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,9 @@ set :pty, true
 set :use_sudo, false
 set :stage, :production
 set :deploy_via, :remote_cache
-set :puma_conf, "#{shared_path}/config/puma.rb"
+set :puma_conf, "#{release_path}/config/puma.rb"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :rbenv_ruby, '2.2.3'
 
 server '46.101.166.85', user: 'backend', roles: %w{app db web}
